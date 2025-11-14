93f33102a36d37bb11bc8b05f07ab790e5a6efec
Hafivah Tahta Rasyida <it.developer@seatrium.com>
2025-11-14 10:34:05 +0700

    first commit

---
+++ src/components/footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="mt-auto">
+      <div className="flex justify-center bg-slate-100 text-black text-sm py-2">
+        <p className="text-xs">© 2024 - SEATRIUM - PCMS MECHANICAL COMPLETION</p>
+      </div>
+    </footer>
+  );
+}
